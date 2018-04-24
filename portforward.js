e86e76811e28b5f1118e182562b4136ccc40720f
Richard Vowles <richard@bluetrainsoftware.com>
2018-04-24 20:50:25 +1200

    - fix bug in int11 relating to click - fix issue with int15 in relation to endpoints for SQS && marketplace - decreased time between runs to 30 seconds for marketplace & whodat updaters - rerun 3, 5, 6, 11

--- portforward.js
+++ portforward.js
@@ -16,18 +16,19 @@ let kubeConfig;
 const runningPods = [];
 
 const path = require('path');
+
 function resolveHome(filepath) {
-    if (filepath[0] === '~') {
-        return path.join(process.env.HOME, filepath.slice(1));
-    }
-    return path;
+	if (filepath[0] === '~') {
+		return path.join(process.env.HOME, filepath.slice(1));
+	}
+	return path;
 }
 
 try {
-    systemDefinedPods = JSON.parse(fs.readFileSync(resolveHome('~/.kube/pods.json'), 'utf-8'));
+	systemDefinedPods = JSON.parse(fs.readFileSync(resolveHome('~/.kube/pods.json'), 'utf-8'));
 }
 catch (e) {
-    systemDefinedPods = {};
+	systemDefinedPods = {};
 }
 
 try {
@@ -39,15 +40,44 @@ catch (e) {
 
 _.merge(systemDefinedPods, localDefinedPods);
 
-function timeLog (line) {
+// turn all formats into this format
+function standardiseConfig(pods) {
+	const toDelete = [];
+
+	for (svc of pods) {
+		if (!_.isObject(pods[svc])) {
+			const port = pods[svc];
+			pods[svc] = {
+				from: port,
+				to: port,
+				namespace: namespace
+			}
+		} else if (pods[svc].to) {
+			pods[svc].from = pods[svc].from || pods[svc].to;
+			pods[svc].namespace = pods[svc].namespace || namespace;
+		} else {
+			toDelete.push(svc);
+		}
+	}
+
+	if (toDelete.length > 0) {
+		console.log('Ignoring pods ', toDelete, 'they are incorrectly configured.');
+		_(toDelete).each((svc) => {
+			delete pods[svc];
+		})
+	}
+}
+
+
+function timeLog(line) {
 	console.log(`[${chalk.grey(timestamp('HH:mm:ss'))}] ${line}`);
 }
 
 let restartAttempt = 0;
 
 function getPods(callback) {
-    exec(`kubectl get pods${ kubeConfig ? ' --kubeconfig ' + kubeConfig : ''}${ namespace ? ' --namespace '+namespace : ''}`, function(error, stdout, stderr){
-        if (error) {
+	exec(`kubectl get pods${ kubeConfig ? ' --kubeconfig ' + kubeConfig : ''}${ namespace ? ' --namespace ' + namespace : ''}`, function (error, stdout, stderr) {
+		if (error) {
 			if (stderr.match(/network is unreachable/) || stderr.match(/handshake timeout/) || stderr.match(/network is down/) || stderr.match(/i\/o timeout/)) {
 				timeLog(`Network error, restarting (${restartAttempt})...`);
 				restartAttempt++;
@@ -59,61 +89,61 @@ function getPods(callback) {
 				});
 				callback(true);
 			} else {
-            	throw stderr;
+				throw stderr;
 			}
-        } else {
-        	if (restartAttempt > 0) {
+		} else {
+			if (restartAttempt > 0) {
 				restartAttempt = 0;
 				timeLog(`Connection established, restarting port forwarding for every pod...`);
 			}
-        	callback(false, stdout, true);
+			callback(false, stdout, true);
 		}
-    });
+	});
 }
 
-function getPodId(rawPods, podName, silent){
+function getPodId(rawPods, podName, silent) {
 	const lines = rawPods.split('\n');
 
 	const filterPods = [];
-	for(let count = 0; count < lines.length; count ++) {
+	for (let count = 0; count < lines.length; count++) {
 		if (lines[count].startsWith(podName)) {
 			filterPods.push(lines[count]);
 		}
 	}
 	const joined = filterPods.join('\n');
-    const match = (new RegExp('('+podName+'[-\\da-z]*).*(Running)', 'g')).exec(joined);
-    if (match) {
-        return match[1];
-    } else {
-        const errorMatch = (new RegExp('('+podName+'[-\\da-z]*)[\\s]*[\\d]\\/[\\d][\\s]*([a-zA-Z]*)[\\s]', 'g')).exec(joined);
-        if (errorMatch) {
+	const match = (new RegExp('(' + podName + '[-\\da-z]*).*(Running)', 'g')).exec(joined);
+	if (match) {
+		return match[1];
+	} else {
+		const errorMatch = (new RegExp('(' + podName + '[-\\da-z]*)[\\s]*[\\d]\\/[\\d][\\s]*([a-zA-Z]*)[\\s]', 'g')).exec(joined);
+		if (errorMatch) {
 			if (silent) {
 				return false;
 			} else {
 				throw `Pod ${chalk.cyan(podName)} is in ${chalk.cyan(errorMatch[2])} state, needs to be ${chalk.cyan('Running')}`;
 			}
-        } else {
+		} else {
 			if (silent) {
 				return false;
 			} else {
-				throw `Could not find pod ${chalk.cyan(podName)}, available pods${ namespace ? ' (namespace: '+namespace+')' : ''}: \n${joined}`;
+				throw `Could not find pod ${chalk.cyan(podName)}, available pods${ namespace ? ' (namespace: ' + namespace + ')' : ''}: \n${joined}`;
 			}
-        }
-    }
+		}
+	}
 }
 
 function portForwardPod(pod) {
-    const child = exec(`kubectl port-forward ${pod.id} ${pod.port}${ kubeConfig ? ' --kubeconfig=' + kubeConfig : ''}${ namespace ? ' --namespace='+namespace : ''}`);
+	const child = exec(`kubectl port-forward ${pod.id} ${pod.from}${pod.to ? ':' + pod.to}${ kubeConfig ? ' --kubeconfig=' + kubeConfig : ''}${ pod.namespace ? ' --namespace=' + pod.namespace : ''}`);
 
-    child.stdout.on('data', function(data) {
-        processKubectlLog(data, pod, child);
-    });
-    child.stderr.on('data', function(data) {
-        processKubectlLog(data, pod, child);
-    });
-    child.on('close', function(code) {
-        //nothing for now
-    });
+	child.stdout.on('data', function (data) {
+		processKubectlLog(data, pod, child);
+	});
+	child.stderr.on('data', function (data) {
+		processKubectlLog(data, pod, child);
+	});
+	child.on('close', function (code) {
+		//nothing for now
+	});
 
 	pod.childProcess = child;
 }
@@ -123,7 +153,7 @@ function healthCheck() {
 		if (error) return;
 		runningPods.forEach(pod => {
 			if (!pod.restartingState) {
-				const match = (new RegExp('('+pod.id+').*(Running)', 'g')).exec(rawPods);
+				const match = (new RegExp('(' + pod.id + ').*(Running)', 'g')).exec(rawPods);
 				if (!match || !pod.initialized) {
 					pod.restartingState = true;
 					pod.childProcess.kill();
@@ -165,18 +195,18 @@ function healthCheck() {
 }
 
 function processKubectlLog(logLine, pod, child) {
-    if (logLine.match(/Forwarding from/)) {
-    	if (!pod.initialized) {
+	if (logLine.match(/Forwarding from/)) {
+		if (!pod.initialized) {
 			pod.initialized = true;
 			timeLog(`Started port forwarding for ${chalk.cyan(pod.name)} on port ${chalk.magenta(pod.port)}`);
 		}
-    } else if (logLine.match(/Handling connection/)) {
+	} else if (logLine.match(/Handling connection/)) {
 		timeLog(`Processing request for ${chalk.cyan(pod.name)}`);
 	} else if (logLine.match(/an error occurred forwarding/)) {
 		timeLog(`Unable to process request for ${chalk.cyan(pod.name)}`);
 		healthCheck();
-    } else if (logLine.match(/address already in use/) || logLine.match(/Unable to listen on any of the requested ports/)) {
-    	if (!pod.excluded) {
+	} else if (logLine.match(/address already in use/) || logLine.match(/Unable to listen on any of the requested ports/)) {
+		if (!pod.excluded) {
 			pod.excluded = true;
 			pod.childProcess.kill();
 			timeLog(`Pod ${chalk.cyan(pod.name)} seems to be already forwarded, excluding...`);
@@ -189,16 +219,16 @@ function processKubectlLog(logLine, pod, child) {
 	} else if (logLine.match(/bind: permission denied/)) {
 		timeLog(chalk.red(`Permission denied to bind ${chalk.cyan(pod.name)} on port ${chalk.magenta(pod.port)}`));
 		process.exit(0);
-	}  else {
-    	if (!pod.initialized) {
-    		// must be true error message
+	} else {
+		if (!pod.initialized) {
+			// must be true error message
 			timeLog(chalk.red(`Failed to initialize port forwarding for ${chalk.cyan(pod.name)} on port ${chalk.magenta(pod.port)}: \n`));
 			console.log(logLine);
 			process.exit();
 		} else {
-			timeLog(`${chalk.blue(pod.name)}: ` +logLine);
+			timeLog(`${chalk.blue(pod.name)}: ` + logLine);
 		}
-    }
+	}
 }
 
 try {
@@ -216,18 +246,22 @@ try {
 		} else if (arg.match(/--kubeconfig=[^\s]*?/)) {
 			kubeConfig = arg.split('=')[1];
 		} else if (arg === '--kubeconfig') {
-			snarf = function(kc) { kubeConfig = kc };
+			snarf = function (kc) {
+				kubeConfig = kc
+			};
 		} else if (arg.match(/--namespace=[a-zA-z]*?/)) {
 			namespace = arg.split('=')[1];
 		} else if (arg === '--namespace') {
-			snarf = function(ns) { namespace = ns; };
+			snarf = function (ns) {
+				namespace = ns;
+			};
 		} else if (arg.match(/--exclude=(?:[\-a-zA-Z]*,?)*/)) {
 			podsToExclude = arg.split('=')[1].split(',').map((pod) => pod.trim());
 		} else if (arg.match(/--health-interval=[\d]*?/)) {
 			healthInterval = arg.split('=')[1];
 		} else if (arg === '--help' || arg === '-help' || arg === '?' || arg == 'help') {
-			console.log( 'kubectlpf:' +
-`
+			console.log('kubectlpf:' +
+				`
 --kubeconfig config-file - override using ~/.kube/config
 --namespace namespace - override the namespace in the kubernetes config file
 --exclude=pods - comma separated values of pods to exclude from forwarding
